d63a96265728c2e108aa0b776f4cc3aec6ce0edf
gayoung46 <gayoung04066@naver.com>
2023-11-09 16:13:43 +0900

    06

--- main.c
+++ main.c
@@ -6,20 +6,21 @@
 int main(int argc, char *argv[])
 {
     FILE* fp;
-    char str[100];
-    int i;
+    char c;
     
-    fp = fopen("sample. txt", "w");
-    
-    for(i=0;i<3;i++)
+    fp = fopen("sample.txt", "r");
+    if(fp == NULL)
     {
-             printf("imput a word: ");
-             scanf("%s", str);
-             fprintf(fp, "%s\n", str);
+          printf("failed to open\n");
+          return 0;
+    }
+    
+    while( (c= fgetc(fp)) !=EOF)
+    {
+           putchar(c);
     }
     
     fclose(fp);
-    
     
   system("PAUSE");	
   return 0;
